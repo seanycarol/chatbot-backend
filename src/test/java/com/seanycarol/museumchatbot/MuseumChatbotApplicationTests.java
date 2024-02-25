19379938cc2f6513186bbdfa0f106c2feb9c8fd9
seanycarol <seany@ice.ufjf.br>
2024-02-25 20:28:52 -0300

    Initial commit

---
+++ src/test/java/com/seanycarol/museumchatbot/MuseumChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seanycarol.museumchatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuseumChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
